ec6ccdea4b75c4169cb0935b474b52f4e85d028c
Joane <138986247+japcunha@users.noreply.github.com>
2025-07-06 18:01:31 -0300

    generics,coringa, HashCode e equals

--- LeitorDeArquivo/Program.java
+++ LeitorDeArquivo/Program.java
@@ -4,8 +4,5 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Program {
-    public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
-        String path = a ;
-    }
+
 }
